ed430fcf8ede0877ea82d74ebcf78d4879ed88ea
tsymbalivaan <ivaaan.tsymbal004@gmail.com>
2023-06-06 22:15:46 +0300

    init

--- src/store/reducers/auth/operations.ts
+++ src/store/reducers/auth/operations.ts
@@ -1,7 +1,8 @@
 import { PayloadAction } from '@reduxjs/toolkit';
+import { User } from 'core/types/user.type';
 import { IinitialState } from 'store/reducers/auth/reducer';
 
-export const signInOperation = (state: IinitialState, { payload }: PayloadAction<any>) => {
+export const signInOperation = (state: IinitialState, { payload }: PayloadAction<User>) => {
 	state.user = payload;
 };
 
